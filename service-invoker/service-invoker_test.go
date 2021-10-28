a5a2e86779c7339495d506f59715f4d9d2ce9e8d
张世琦 <zhangsq@zhangshiqideMacBook-Pro.local>
2021-10-28 14:35:41 +0800

    test: 增加 service-invoker 测试代码

---
+++ service-invoker/service-invoker_test.go
@@ -0,0 +1,63 @@
+package service_invoker
+
+import (
+	"github.com/zhangsq-ax/aliyun-iot-server-client/options"
+	"os"
+	"testing"
+)
+
+var invoker *ServiceInvoker
+
+var (
+	regionId        string
+	accessKeyId     string
+	accessKeySecret string
+	instanceId      string
+	productKey      string
+	deviceName      string
+)
+
+func init() {
+	regionId = os.Getenv("IOT_REGION")
+	accessKeyId = os.Getenv("IOT_ACCESS_KEY_ID")
+	accessKeySecret = os.Getenv("IOT_ACCESS_KEY_SECRET")
+	instanceId = os.Getenv("IOT_INSTANCE_ID")
+	productKey = os.Getenv("IOT_PRODUCT_KEY")
+	deviceName = os.Getenv("IOT_DEVICE_NAME")
+}
+
+func TestNewServiceInvoker(t *testing.T) {
+	var err error
+	invoker, err = NewServiceInvoker(&options.ServiceInvokerOptions{
+		RegionID:        regionId,
+		AccessKeyID:     accessKeyId,
+		AccessKeySecret: accessKeySecret,
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func TestServiceInvoker_Invoke(t *testing.T) {
+	res, err := invoker.Invoke(&options.DeviceInfo{
+		IotInstanceID: instanceId,
+		ProductKey:    productKey,
+		DeviceName:    deviceName,
+	}, options.NewServiceRequestPayload("thing.service.reserve", map[string]interface{}{
+		"order_id": "test-order",
+		"detail": []map[string]int{
+			{
+				"goods_id": 2468,
+				"amount":   2,
+			},
+		},
+	}))
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	t.Log(res)
+}
