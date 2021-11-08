172ca13c26745b18bfebaea840e7cc71f26326eb
张世琦 <zhangsq@zhangshiqideMacBook-Pro.local>
2021-11-08 14:44:25 +0800

    feat: 服务器响应增加业务数据字段

--- options/service-response.go
+++ options/service-response.go
@@ -7,8 +7,9 @@ import (
 
 // ServiceResponse 服务响应
 type ServiceResponse struct {
-	ID   string `json:"id"`   // 响应标识
-	Code int    `json:"code"` // 响应状态标识，详情参考 https://help.aliyun.com/document_detail/89309.htm
+	ID   string      `json:"id"`             // 响应标识
+	Code int         `json:"code"`           // 响应状态标识，详情参考 https://help.aliyun.com/document_detail/89309.htm
+	Data interface{} `json:"data,omitempty"` // 响应参数
 }
 
 // NewServiceResponseByBase64 通过 Base64 字符串创建服务响应对象
@@ -17,7 +18,6 @@ func NewServiceResponseByBase64(base64Payload string) (*ServiceResponse, error) 
 	if err != nil {
 		return nil, err
 	}
-	//fmt.Println(string(b))
 
 	resPayload := &ServiceResponse{}
 	err = json.Unmarshal(b, resPayload)
